805f744eed9c335b9a9f382af7d7c5ca0d81d769
DESKTOP-3N56K9C\LG <jypark@ssenstone.com>
2024-03-04 15:59:14 +0900

    list,array stack

---
+++ list_stack.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef STACK_H
+#define LIST_STACK_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <vld.h>
+#include <string.h>
+//stack with list
+#define MAX 100
+struct STACK {
+	char data;
+	struct STACK* next;
+}typedef Stack;
+
+
+void init();
+void push(char data);
+Stack* make_stack(char data);
+char pop();
+char peek();
+int is_empty();
+int is_full();
+int read_line(FILE*, char*, int);
+void process_command();
+void show_stack();
+void clear_stack();
+#endif
---
+++ stack.h
@@ -0,0 +1,18 @@
+//#pragma once
+//#ifndef LIST_STACK_H
+//#define STACK_H
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <vld.h>
+//#include <string.h>
+//void push(char ch);
+//char pop();
+//char peek();
+//int is_full();
+//int is_empty();
+//void show_stack();
+//void process_command();
+// int read_line(FILE* fp, char* buf, int limit);
+//#endif
